fddeaf4b19e523be1a35c8be91dff8f5ab277391
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-07 12:09:42 +0100

    dev: added serve-index

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 console.log("About to start a server...");
 
 const express = require("express");
+const serveIndex = require("serve-index");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static("."));
+app.use(serveIndex(".", { icons: true }));
 
 app.listen(3000, () => {
   console.log("Server started with success on port 3000");
