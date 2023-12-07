6f9a90d6420b350d676df14fcc7df77b337d6950
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-12-07 11:44:49 +0100

    dev: added first api

--- back/server.js
+++ back/server.js
@@ -4,6 +4,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/toto", (req, res) => {
+  res.json({ toto: 123 });
+});
+
 app.listen(3000, () => {
   console.log("Server started with success on port 3000");
 });
